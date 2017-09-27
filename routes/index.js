30e0027960061a2917b962ee762b75273ccdb472
Jedai Saboteur <dev@paradox.inversion.com>
2017-09-26 17:47:26 -0700

    Updated Styles

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,20 @@
 const fs = require("fs");
 var express = require('express');
 var router = express.Router();
+const getParts = function(){
+  var urlArray = [];
+  var seriesData = JSON.parse(fs.readFileSync(__dirname + '/../public/series/seriesData.json', "utf8"));
 
+  seriesData.forEach(function(element){
+    urlArray.push(element);
+  });
+  return urlArray;
+};
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var allParts = getParts();
   res.render('index', { title: 'Series', parts: allParts});
 });
-function getParts(){
-  var urlArray = [];
-  var seriesData = JSON.parse(fs.readFileSync(__dirname + '/../public/series/seriesData.json', "utf8"));
-  // seriesData.forEach(function(element){
-  //   urlArray.push(element.content);
-  // });
-  seriesData.forEach(function(element){
-    urlArray.push(element);
-  });
-  console.log(seriesData);
-  console.log(urlArray);
-  return urlArray;
-}
+
+
 module.exports = router;
--- routes/reader.js
+++ routes/reader.js
@@ -1,20 +1,26 @@
 var express = require('express');
 var fs = require('fs');
-var path = require("path");
 var router = express.Router();
 var currentEntry = 0;
 /* GET reader page. */
 router.get('/', function(req, res, next) {
-  console.log(process.argv);
-  var testData = fs.readFileSync(__dirname + '/../public/series/seriesData.json');
+  if (req.query.part){
+    currentEntry = req.query.part;
+    const data = JSON.parse(fs.readFileSync(__dirname + '/../public/series/seriesData.json'));
 
-  var data = JSON.parse(testData);
-  res.render('page',
-    {
-      author: data[currentEntry].author,
-      storyTitle: data[currentEntry]["entry-name"],
-      content: fs.readFileSync(__dirname + "/../public/series/" + data[currentEntry].content, "utf8")
-    });
+    res.render('page',
+      {
+        author: data[currentEntry].author,
+        storyTitle: data[currentEntry]["entry-name"],
+        content: fs.readFileSync(__dirname + "/../public/series/" + data[currentEntry].content, "utf8"),
+        part: currentEntry,
+        totalParts: data.length
+      });
+  } else{
+    //Redirect if there's no query string
+    res.redirect("?part=0");
+  }
+
 });
 
 
